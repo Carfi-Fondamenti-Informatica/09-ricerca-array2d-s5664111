a38dfa393dc49d92130b6f37b6f0f58c1eee3762
s5664111 <117274748+s5664111@users.noreply.github.com>
2022-12-17 10:33:08 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,16 @@
 
+bool f(char d2[10][20], char d1[20],int &i){
+    for(int x=0; x<10; x++){
+        for(int y=0; y<20; y++){
+            if(d2[x][y]!=d1[y]){
+                y=20;
+            }else{
+                if(y==19){
+                    i=x;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
